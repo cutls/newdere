7dd2af31b650040e40ba1df80ac1a8d2423528b4
Cutls <web-pro@cutls.com>
2022-10-05 11:39:52 +0900

    Update getImage.ts

--- src/getImage.ts
+++ src/getImage.ts
@@ -5,9 +5,13 @@ import sheet from './sheet'
 import create from './create'
 
 export default async function main () {
+    try{
     const idols = JSON.parse(fs.readFileSync('allCharaIdData.json').toString())
     const sheetData = await sheet()
     const result = await calc(sheetData, idols)
     await create(result)
+    } catch (e) {
+      console.error(e)
+    }
 }
 main()
