4fa7cb60efbc6ebff5d00be5d1d4d189dfa9cd7c
cutls <web-pro@cutls.com>
2022-10-25 03:23:28 +0900

    one get image component

--- src/getImage.ts
+++ src/getImage.ts
@@ -7,12 +7,13 @@ import create from './create'
 export default async function main() {
   try {
     const cv = false
+    const tweet = true // tweetがtrueならdebugが消えて、GCSにアップロードされる
     const idols = JSON.parse(fs.readFileSync('allCharaIdData.json').toString())
     const sheetData = await sheet(cv)
     const result = await calc(sheetData, idols)
-    const {buffer, url} = await create(result, true, !cv)
+    const { buffer, url } = await create(result, !tweet, !cv)
     const status = `更新情報test ${url}`
-    await twitter(status, buffer)
+    if (tweet) await twitter(status, buffer)
   } catch (e) {
     console.error(e)
   }
