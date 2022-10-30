49f965ef2a45520dd70626df7fbe6387b0b42e25
cutls <web-pro@cutls.com>
2022-10-30 17:56:03 +0900

    add break

--- src/task.ts
+++ src/task.ts
@@ -31,7 +31,7 @@ export default async function main() {
     const sheetData = await sheet(cv)
     const result = await calc(sheetData, idols)
     const { buffer, url } = await create(result, false, !cv)
-    const status = `デレステガシャ更新${br}${notation.join(br)}${br}高画質版: ${url}`
+    const status = `デレステガシャ更新${br}${br}${notation.join(br)}${br}${br}高画質版: ${url}`
     await twitter(status, buffer)
 }
 main()
